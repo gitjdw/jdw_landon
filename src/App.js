da8e5784a2f5781697da63923e7f9d90c2428fe4
Jarod W <jarod@amazon.com>
2022-07-19 12:30:08 -0400

    updated App.js message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React in less than a day!
+          Learn React in less than a day! And get it online!
         </a>
       </header>
     </div>
