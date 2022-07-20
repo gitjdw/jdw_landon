f46c6cea140d489a37b66ebd0f878dd458fe92df
Jarod W <jarod@amazon.com>
2022-07-20 12:27:54 -0400

    Added JSON to represent data and made components dynamic

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Welcome from './Welcome.js';
+import Hotelinfo from './Hotelinfo.js';
+
+function Main() {
+    return (
+        <main id="wrapper">
+
+            <Welcome />
+
+            <Hotelinfo />
+
+        </main>
+    );
+}
+export default Main;
